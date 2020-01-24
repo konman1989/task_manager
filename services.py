434549ca8150f3b680ae62a98905b6de4698b654
konman1989 <manoilo.blog@gmail.com>
2020-01-24 17:08:23 +0200

    rabbitmq notifications partly implemented

--- services.py
+++ services.py
@@ -1,18 +1,26 @@
 import json
 import pika
+from typing import NamedTuple
 
 
-def init_task_creation(task):
+class Message(NamedTuple):
+    event_name: str
+    event: str
+
+
+def init_event_creation(name: str, event: dict) -> None:
+    message = Message(event_name=name, event=str(event))
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='127.0.0.1'))
+
     channel = connection.channel()
 
-    channel.queue_declare(queue='task')
+    channel.queue_declare(queue='message')
 
     channel.basic_publish(
         exchange='',
-        routing_key='task',
-        body=json.dumps(task),
+        routing_key='message',
+        body=json.dumps(message),
         properties=pika.BasicProperties(
             delivery_mode=2
         )
